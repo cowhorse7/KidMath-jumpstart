f71245a1a6f6383f5830924ccd1f32acd200718b
cowhorse7 <becca11175@gmail.com>
2025-06-11 22:31:07 -0600

    please just let me look at my dashboard again

--- generate-env.js
+++ generate-env.js
@@ -7,7 +7,6 @@ export const environment = {
   production: true,
   SUPABASE_URL: '${process.env.SUPABASE_URL}',
   SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY}',
-  SUPABASE_JWT_SECRET: '${process.env.SUPABASE_JWT_SECRET}',
 };
 `;
 fs.writeFileSync(targetPath, envConfigFile);
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  SUPABASE_URL: 'https://mdjgqxfsdosxwwjkdonj.supabase.co',
+  SUPABASE_ANON_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1kamdxeGZzZG9zeHd3amtkb25qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1NDYxOTksImV4cCI6MjA2NDEyMjE5OX0.D2EFkB8Ak7R4CSQOGcrTPsE26OboviTC46S04Mnrk0E',
+};
